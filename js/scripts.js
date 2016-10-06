a9a2e01be0fae330a736c30ccf7ba69251afbaa1
Terri Bradley <terribradley@gmail.com>
2016-10-06 12:07:52 -0700

    nav

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,28 @@ $("document").ready(function() {
 	$(".intro-link").click(function(){
 
 		$("html, body").animate({
-			scrollTop: $("#overview").offset().top
+			scrollTop: $(".overview").offset().top
 		}, 2000);
 	});
-	$(".context-link").click(function(){
+	$(".concept-link").click(function(){
 
 		$("html, body").animate({
-			scrollTop: $("#context").offset().top
-		}, 2000);
-	});
-	$(".process-link").click(function(){
-
-		$("html, body").animate({
-			scrollTop: $("#process").offset().top
+			scrollTop: $(".concept").offset().top
 		}, 2000);
 	});
 	$(".solution-link").click(function(){
 
 		$("html, body").animate({
-			scrollTop: $("#solution").offset().top
+			scrollTop: $(".solution").offset().top
 		}, 2000);
 	});
 	$(".result-link").click(function(){
 
 		$("html, body").animate({
-			scrollTop: $("#result").offset().top
+			scrollTop: $(".result").offset().top
 		}, 2000);
 	});
+
 
 	// $(".headline").scrollPath({
 	// 	drawPath: true,
